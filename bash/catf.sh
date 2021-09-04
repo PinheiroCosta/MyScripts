9991c17344963c0dcf069ffd9e14c713dd2a9ce0
Rômulo <romulopinheirocosta@gmail.com>
2021-09-04 17:48:00 -0300

    Add missing argument error treatment

--- bash/catf.sh
+++ bash/catf.sh
@@ -1,35 +1,53 @@
 #!/usr/bin/env bash
 
+
 catf() {
-    # Search for a  bash function inside the document
-    # Usage: catf function_name filename
-    # insert the name of the function without the parenthesis 
-    # ex: catf instead of catf().
+		# Search for a function inside the input script by its name
+		# Usage: catf function_name filename
+		# Only works if the script is pre formated with the ending comment containing the function name.
+		
+		# Formatting
+		RESET="\e[m";
+		GREEN="\e[32m";
+		YELLOW="\e[33m";
+		
+		# parameters
+		function=$1;
+		filename=$2;
+		
+		# Error messages
+		notFound="$function not found in $filename.";
+		usage="catf functionName filename.sh";
+	
+		# if any parameter is missing show how to use the program in green and exit
+		if [ ! "$function" ] || [ ! "$filename" ]; then
+			printf "You should inform two arguments.\n"
+			printf "$YELLOW\rusage: %s$RESET\n" "$usage";
+		else
+			# get the starting line of the function
+			startLine=$(grep -nsm 1 "$function \?() \?{" "$filename" | \
+				cut -d ':' -f 1);
+			
+			# if command runs successfuly
+			if [ "$startLine" ]; then
+					# get the ending line of the function
+					endingLine=$(sed -n "$startLine,$ p" "$filename" | \
+						grep -nsm 1 "}[[:space:]]" | \
+						cut -d ':' -f 1);
+					# if found the complete body of the  function
+					if [ "$endingLine" ]; then
+							# echo the body of the function	
+							functionText=$(head -"$((startLine + endingLine))" "$filename" | \
+								tail -"$((endingLine + 1))");
+							echo -e "$functionText";
+					else
+						echo "function $function() was found in the document, but it couldn't be retrieved."
+					fi;
+			else
+					echo "$notFound"
+		fi;
+	
 
-    function=$1;
-    filename=$2;
-    error="$function not found in $filename."
+		fi
 
-    # get the starting line number of the function
-    startLine=$(grep -nm 1 "$function \?() \?{" "$filename" | \
-    cut -d ':' -f 1);
-
-    # if command runs successfuly
-    if [ "$startLine" ]; then
-        # get the ending line number of the function
-        endingLine=$(sed -n "$startLine,$ p" "$filename" | \
-        grep -nm 1 "}[[:space:]]" | \
-        cut -d ':' -f 1);
-        # if found the complete body of the function
-        if [ "$endingLine" ]; then
-            # echo the body of the function	
-            functionText=$(head -"$((startLine + endingLine))" "$filename" | \
-            tail -"$((endingLine + 1))");
-            echo "$functionText";
-        else
-            echo "$error"
-        fi;
-    else
-        echo "$error"
-    fi;
-}   # --------------------------------- end of catf() function ------------------------------
+}		# --------------------------------- end of catf() function ------------------------------
